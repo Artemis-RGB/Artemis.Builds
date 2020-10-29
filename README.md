c37afd202910b0a6d5b2ed6f1a4d981cf33d9c0a
Robert <mail@rbeekman.nl>
2020-10-29 09:24:56 +0100

    Added initial codebase
    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Artemis.Builds
+# Artemis Build fetcher
+[![GitHub license](https://img.shields.io/badge/license-GPL3-blue.svg)](https://github.com/Artemis-RGB/Artemis.Builds/blob/master/LICENSE)
+[![GitHub stars](https://img.shields.io/github/stars/Artemis-RGB/Artemis.Plugins.svg)](https://github.com/Artemis-RGB/Artemis.Builds/stargazers)
+[![Discord](https://img.shields.io/discord/392093058352676874?logo=discord&logoColor=white)](https://discord.gg/S3MVaC9) 
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VQBAEJYUFLU4J) 
+
 A simple web application that fetches the latest build artifacts for the given build definition
+
+#### Want to build? Follow these instructions
+1. Clone the repository
+2. Restore NuGet packages
+3. Open `src\appsettings.json` and replace `<insert the token here>` with your DevOps access token
+4. Optionally also replace the value of `ApiBaseUrl`  depending on which organisation and project you want to access.
---
+++ src/Controllers/BuildController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace Artemis.Build.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BuildController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public BuildController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{buildDefinition}/{artifactName}")]
+        public async Task<RedirectResult> Get(int buildDefinition, string artifactName)
+        {
+            var apiBaseUrl = _configuration["ApiBaseUrl"];
+            var personalAccessToken = _configuration["PersonalAccessToken"];
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Basic",
+                Convert.ToBase64String(Encoding.ASCII.GetBytes($":{personalAccessToken}"))
+            );
+
+            // Get the latest build ID
+            using var latestBuildResponse = await client.GetAsync($"{apiBaseUrl}build/latest/{buildDefinition}?branchName=master&api-version=6.1-preview.1");
+            latestBuildResponse.EnsureSuccessStatusCode();
+            var latestBuildBody = await latestBuildResponse.Content.ReadAsStringAsync();
+
+            var latestBuildJson = JObject.Parse(latestBuildBody);
+            var id = latestBuildJson["id"].Value<int>();
+
+            // Get the artifacts of the latest ID
+            using var artifactResponse = await client.GetAsync($"{apiBaseUrl}build/builds/{id}/artifacts?artifactName={artifactName}&api-version=6.1-preview.5");
+            artifactResponse.EnsureSuccessStatusCode();
+            var artifactBody = await artifactResponse.Content.ReadAsStringAsync();
+
+            var artifactJson = JObject.Parse(artifactBody);
+            return Redirect(artifactJson["resource"]["downloadUrl"].Value<string>());
+        }
+    }
+}
